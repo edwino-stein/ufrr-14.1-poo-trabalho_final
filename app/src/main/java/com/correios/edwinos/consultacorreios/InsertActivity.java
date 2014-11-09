b54bc474a9194451618a148207dbd4c056929c65
Edwino Stein <edwino@m2smart.com.br>
2014-11-09 16:12:49 -0400

    Adicionando atividade de registro de objetos

---
+++ app/src/main/java/com/correios/edwinos/consultacorreios/InsertActivity.java
@@ -0,0 +1,77 @@
+package com.correios.edwinos.consultacorreios;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class InsertActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_insert, menu);
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onOkBtnClicked(View v){
+        String frendlyName = ((EditText) findViewById(R.id.frendlyName)).getText().toString();
+        String codeObj = ((EditText) findViewById(R.id.codeObj)).getText().toString();
+
+        if(!this.codeObjIsValid(codeObj)){
+            Toast.makeText(this, "O código informado é inválido", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Intent data = new Intent();
+
+            data.putExtra("name", frendlyName);
+            data.putExtra("code", codeObj);
+            setResult(RESULT_OK, data);
+
+            finish();
+        }
+    }
+
+    public void onCancelBtnClicked(View v){
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+
+    protected boolean codeObjIsValid(String code){
+
+        /**
+         * TODO: Fazer a validação do codigo de objeto
+         */
+
+        if(code.length() == 3) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
