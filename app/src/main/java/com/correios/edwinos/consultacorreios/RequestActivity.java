b7ef33491ea669f69b57ddfa65e2ccb60217adce
Edwino Stein <edwino@m2smart.com.br>
2014-11-15 14:52:44 -0400

    [DEBUB] Adicionando codigo para debug em InsertActivity e RequestActivity #2

--- app/src/main/java/com/correios/edwinos/consultacorreios/RequestActivity.java
+++ app/src/main/java/com/correios/edwinos/consultacorreios/RequestActivity.java
@@ -20,14 +20,15 @@ public class RequestActivity extends Activity implements HttpCallback{
 
         this.code = getIntent().getStringExtra("code");
 
-        if(code != null) {
-
+        if(code != null && !code.isEmpty()) {
             /** DEBUB **/
             this.beforeSendCallback();
             this.afterSendCallback();
             HttpResponse response = new HttpResponse();
             response.statusCode = 200;
             response.responseText = "{\"success\":true,\"data\":[{\"data\":{\"date\":\"2014-10-15 16:51:00\",\"timezone_type\":3,\"timezone\":\"America\\/Sao_Paulo\"},\"local\":\"CEE BOA VISTA - BOA VISTA\\/RR\",\"situacao\":\"Objeto entregue ao destinat\\u00e1rio\",\"estado\":1,\"id\":3},{\"data\":{\"date\":\"2014-10-15 09:22:00\",\"timezone_type\":3,\"timezone\":\"America\\/Sao_Paulo\"},\"local\":\"CEE BOA VISTA - BOA VISTA\\/RR\",\"situacao\":\"Objeto saiu para entrega ao destinat\\u00e1rio\",\"estado\":2,\"id\":2},{\"data\":{\"date\":\"2014-10-13 17:00:00\",\"timezone_type\":3,\"timezone\":\"America\\/Sao_Paulo\"},\"local\":\"CTE CAMPINAS\\/GCCAP - VALINHOS\\/SP\",\"situacao\":\"Objeto encaminhado\",\"detalhes\":\"Em tr\\u00e2nsito para CEE MANAUS - MANAUS\\/AM\",\"estado\":0,\"id\":1},{\"data\":{\"date\":\"2014-10-13 08:48:00\",\"timezone_type\":3,\"timezone\":\"America\\/Sao_Paulo\"},\"local\":\"CTE CAMPINAS\\/GCCAP - VALINHOS\\/SP\",\"situacao\":\"Objeto postado\",\"estado\":0,\"id\":0}],\"total\":4,\"engineInfo\":{\"projectName\":\"Traquer Track Engine\",\"version\":\"0.5\",\"stability\":\"ALPHA\",\"license\":\"Undefined\",\"credits\":[{\"name\":\"Edwino Stein\",\"email\":\"linkxtremer@gmail.com\"}]}}";
+            //response.responseText = "{\"success\":true,\"data\":[],\"total\":0,\"engineInfo\":{\"projectName\":\"Traquer Track Engine\",\"version\":\"0.5\",\"stability\":\"ALPHA\",\"license\":\"Undefined\",\"credits\":[{\"name\":\"Edwino Stein\",\"email\":\"linkxtremer@gmail.com\"}]}}";
+            //response.responseText = "{\"success\":false,\"message\":\"Nenhum c\\u00f3digo de rastreamento v\\u00e1lido foi informado\"}";
             this.successCallback(response);
             /***********/
 
@@ -35,13 +36,12 @@ public class RequestActivity extends Activity implements HttpCallback{
             //request.send("http://192.168.2.2/correios/query/json/"+this.code);
         }
         else {
-
             /** DEBUB **/
             this.beforeSendCallback();
             this.afterSendCallback();
             HttpResponse response = new HttpResponse();
             response.statusCode = 500;
-            response.responseText = "{\"success\":false,\"message\":\"Nenhum c\\u00f3digo de rastreamento v\\u00e1lido foi informado\"}";
+            response.responseText = "Erro interno do servidor";
             this.errorCallback(response);
             /***********/
 
