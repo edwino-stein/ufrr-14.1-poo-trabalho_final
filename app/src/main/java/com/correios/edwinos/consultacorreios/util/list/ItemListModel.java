a82390df23849353cd7a96c74c956a084b059351
Edwino Stein <edwino@m2smart.com.br>
2014-11-16 01:43:20 -0400

    Adicionando valor Id ao ItemListModel

--- app/src/main/java/com/correios/edwinos/consultacorreios/util/list/ItemListModel.java
+++ app/src/main/java/com/correios/edwinos/consultacorreios/util/list/ItemListModel.java
@@ -1,15 +1,32 @@
 package com.correios.edwinos.consultacorreios.util.list;
 
+import com.correios.edwinos.consultacorreios.util.database.CorreiosEntity;
+
 /**
  * Created by EdwinoS on 08/11/14.
  */
+
+/**
+ * TODO: Adicionar ultima situação, hora, data e estado
+ */
+
 public class ItemListModel {
+
+    protected int id;
+
     protected String code;
     protected String frendlyName;
 
+
     public ItemListModel(String code, String frendlyName) {
         this.code = code;
-        this.frendlyName = frendlyName;
+        this.frendlyName = frendlyName.isEmpty() ? code : frendlyName;
+    }
+
+    public ItemListModel(CorreiosEntity entity) {
+        this(entity.getCode(), entity.getName());
+
+        this.id = entity.getId();
     }
 
     public String getCode() {
@@ -26,5 +43,13 @@ public class ItemListModel {
 
     public void setFrendlyName(String frendlyName) {
         this.frendlyName = frendlyName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
